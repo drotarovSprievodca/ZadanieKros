41f7b62b1d9663f27bbbf9e123da44a1756ce4ac
drotarovSprievodca <labajpeter98@gmail.com>
2021-07-09 12:43:58 +0200

    Inicializacia

---
+++ RESTAPI/Controllers/HierarchiaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using FirmaModel;
+
+namespace RESTAPI.Controllers
+{
+    public class HierarchiaController : ApiController
+    {
+        public IEnumerable<hierarchia> GetHierarchia()
+        {
+            using (firmaEntities f = new firmaEntities())
+            {
+                f.Configuration.ProxyCreationEnabled = false;
+                return f.hierarchia.ToList();
+            }
+        }
+    }
+}
---
+++ RESTAPI/Controllers/ZamestnanciController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using FirmaModel;
+
+namespace RESTAPI.Controllers
+{
+    public class ZamestnanciController : ApiController
+    {
+        public IEnumerable<zamestnanci> GetZamestnanci()
+        {
+            using (firmaEntities f = new firmaEntities()) {
+                f.Configuration.ProxyCreationEnabled = false;
+                return f.zamestnanci.ToList();
+            }
+        }
+    }
+}
