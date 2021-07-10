1ecdf4e2d7ad0b5084200cfd9f84dce89efe2c12
drotarovSprievodca <labajpeter98@gmail.com>
2021-07-10 21:05:55 +0200

    kontroler zamestnancov hotovy

--- RESTAPI/Controllers/ZamestnanciController.cs
+++ RESTAPI/Controllers/ZamestnanciController.cs
@@ -10,11 +10,59 @@ namespace RESTAPI.Controllers
 {
     public class ZamestnanciController : ApiController
     {
+        [HttpGet]
         public IEnumerable<zamestnanci> GetZamestnanci()
         {
             using (firmaEntities f = new firmaEntities()) {
                 f.Configuration.ProxyCreationEnabled = false;
                 return f.zamestnanci.ToList();
+            }
+        }
+        [HttpPost]
+        public void PostZamestnanci(zamestnanci zamestnanec)
+        {
+            using (firmaEntities f = new firmaEntities())
+            {
+                f.zamestnanci.Add(new zamestnanci() {
+                    id = zamestnanec.id,
+                    titul = zamestnanec.titul,
+                    meno = zamestnanec.meno,
+                    priezvisko = zamestnanec.priezvisko,
+                    telefon = zamestnanec.telefon,
+                    email = zamestnanec.email
+                });
+                f.SaveChanges();
+            }
+        }
+        [HttpPut]
+        public void PutZamestnanci(int id, zamestnanci upravovany)
+        {
+            using (firmaEntities f = new firmaEntities())
+            {
+                var najdeny = f.zamestnanci.FirstOrDefault(z => z.id == id);
+                if (najdeny != null)
+                {
+                    najdeny.titul = upravovany.titul;
+                    najdeny.meno = upravovany.meno;
+                    najdeny.priezvisko = upravovany.priezvisko;
+                    najdeny.telefon = upravovany.telefon;
+                    najdeny.email = upravovany.email;
+
+                    f.SaveChanges();
+                }
+            }
+        }
+        [HttpDelete]
+        public void DeleteZamestnanci(int id)
+        {
+            using (firmaEntities f = new firmaEntities())
+            {
+                var mazany = f.zamestnanci.FirstOrDefault(z => z.id == id);
+                if (mazany != null)
+                {
+                    f.zamestnanci.Remove(mazany);
+                    f.SaveChanges();
+                }
             }
         }
     }
